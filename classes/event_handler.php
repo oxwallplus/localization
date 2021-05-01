86063ba811e277da09d7c0e237e6244eff13ae8a
toar86 <kontakt@artmedia.biz.pl>
2021-05-01 22:26:37 +0200

    user can select timezone

--- classes/event_handler.php
+++ classes/event_handler.php
@@ -15,30 +15,55 @@ class LOCALIZATION_CLASS_EventHandler
     
     private static $classInstance;
 
-    public static function getInstance() {
-        if(self::$classInstance === null) {
+    public static function getInstance()
+    {
+        if (self::$classInstance === null) {
             self::$classInstance = new self();
         }
 
         return self::$classInstance;
     }
     
-    public function __construct() {
+    public function __construct()
+    {
         $this->key = LOCALIZATION_BOL_Service::KEY;
         $this->eventManager = OW::getEventManager();
     }
     
-    public function genericInit() {
+    public function genericInit()
+    {
         $this->eventManager->bind(OW_EventManager::ON_FINALIZE, [$this, 'onFinalize']);
+        $this->eventManager->bind(BOL_PreferenceService::PREFERENCE_ADD_FORM_ELEMENT_EVENT, [$this, 'onPreferenceAddFormElement']);
     }
 
-    public function init() {
+    public function init()
+    {
         $this->genericInit();
     }
     
-    public function onFinalize() {
+    public function onFinalize()
+    {
         $document = OW::getDocument();
         $document->addStyleSheet(OW::getPluginManager()->getPlugin($this->key)->getStaticCssUrl() . 'famfamfam-flags.css', 'all', -100);
     }
 
+    public function onPreferenceAddFormElement(BASE_CLASS_EventCollector $event)
+    {
+        $language = OW::getLanguage();
+
+        $params = $event->getParams();
+        $values = $params['values'];
+
+        $elements = [];
+
+        $timeZoneSelect = new Selectbox("timeZoneSelect");
+        $timeZoneSelect->setLabel($language->text($this->key, 'timezone'));
+        $timeZoneSelect->addOptions(UTIL_DateTime::getTimezones());
+
+        $timeZoneSelect->setValue($values['timeZoneSelect']);
+
+        $elements[] = $timeZoneSelect;
+
+        $event->add($elements);
+    }
 }
